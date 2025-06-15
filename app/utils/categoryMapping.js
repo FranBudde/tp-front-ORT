b25f4487691fcc99d5ad69a34c369dba7106e31b
santiagokhazki <santiago.khazki@uala.com.ar>
2025-06-14 22:27:10 -0300

    home terminado

---
+++ app/utils/categoryMapping.js
@@ -0,0 +1,25 @@
+const categoryMapping = {
+    food: {
+        id: "food",
+        name: "Food", 
+        icon: "🍔", 
+        color: "#22c55e", 
+        bgColor: "bg-green-500"
+    },
+    groceries: {
+        id: "groceries",
+        name: "Groceries",
+        icon: "🛒", 
+        color: "#3b82f6", 
+        bgColor: "bg-blue-500"
+    },
+    other: {
+        id: "other",
+        name: "Other",
+        icon: "❓", 
+        color: "#fbbf24", 
+        bgColor: "bg-yellow-500"
+    }
+}
+
+export default categoryMapping;
