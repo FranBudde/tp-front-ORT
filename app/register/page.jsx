b35907f9deb7649de68599b69ce23f905f1e4d52
gabyvg16 <gaby.ang16@gmail.com>
2025-06-21 20:53:53 -0300

    Validaciones Registro de usuario

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -4,49 +4,87 @@ import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const router = useRouter();
-  // en la parte superior de tu componente:
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     userName: "",
-    password: ""
+    password: "",
   });
 
   const [error, setError] = useState("");
 
+  const validateForm = () => {
+    const { firstName, lastName, userName, password } = formData;
+
+    if (!firstName || !lastName || !userName || !password) {
+      setError("Todos los campos son obligatorios.");
+      return false;
+    }
+
+    if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(firstName)) {
+      setError("El nombre solo puede contener letras.");
+      return false;
+    }
+
+    if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(lastName)) {
+      setError("El apellido solo puede contener letras.");
+      return false;
+    }
+
+    if (!/^[a-zA-Z0-9_]+$/.test(userName)) {
+      setError(
+        "El nombre de usuario sólo puede contener letras, números y guiones bajos."
+      );
+      return false;
+    }
+
+    if (password.length < 8) {
+      setError("La contraseña debe tener al menos 8 caracteres.");
+      return false;
+    }
+
+    setError("");
+    return true;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
+    if (!validateForm()) return;
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/user/insert_user`,
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/user/insert_user`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(formData)
+          body: JSON.stringify(formData),
         }
-      )
+      );
 
       if (!response.ok) {
-        console.log(response)
-        throw new Error("Error al registrarse");
+        if (response.status === 409) {
+          setError("El nombre de usuario ya está registrado.");
+        } else {
+          setError("Error al registrarse.");
+        }
+        return;
       }
 
+      alert("Tu usuario fue creado exitosamente, por favor, inicia sesión.");
       router.push("/login");
-
     } catch (err) {
       setError(err.message || "Error al conectar con el servidor");
     }
-
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prev => ({
+    setFormData((prev) => ({
       ...prev,
-      [name]: value
+      [name]: value,
     }));
-  }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
@@ -58,21 +96,24 @@ export default function LoginPage() {
         </div>
 
         {error && (
-          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
             <span className="block sm:inline">{error}</span>
-          </div>)
-        }
+          </div>
+        )}
 
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <div className="rounded-md shadow-sm space-y-4">
-
             <div>
-              <label htmlFor="firstName" className="sr-only">Nombre</label>
+              <label htmlFor="firstName" className="sr-only">
+                Nombre
+              </label>
               <input
                 id="firstName"
                 name="firstName"
                 type="text"
-                required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Nombre"
                 onChange={handleChange}
@@ -80,12 +121,13 @@ export default function LoginPage() {
             </div>
 
             <div>
-              <label htmlFor="lastName" className="sr-only">Apellido</label>
+              <label htmlFor="lastName" className="sr-only">
+                Apellido
+              </label>
               <input
                 id="lastName"
                 name="lastName"
                 type="text"
-                required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Apellido"
                 onChange={handleChange}
@@ -93,25 +135,27 @@ export default function LoginPage() {
             </div>
 
             <div>
-              <label htmlFor="userName" className="sr-only">Usuario</label>
+              <label htmlFor="userName" className="sr-only">
+                Usuario
+              </label>
               <input
                 id="userName"
                 name="userName"
                 type="text"
-                required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Nombre de usuario"
                 onChange={handleChange}
               />
             </div>
             <div>
-              <label htmlFor="password" className="sr-only">Contraseña</label>
+              <label htmlFor="password" className="sr-only">
+                Contraseña
+              </label>
               <input
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
-                required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Contraseña"
                 onChange={handleChange}
@@ -122,15 +166,19 @@ export default function LoginPage() {
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed">
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed"
+            >
               Registrarse
             </button>
           </div>
 
           <div className="text-center">
             <p className="text-sm text-gray-600">
-              ¿Ya tenes una cuenta?{' '}
-              <a href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
+              ¿Ya tenes una cuenta?{" "}
+              <a
+                href="/login"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
                 Loguearse
               </a>
             </p>
